05c87106343b4763d565724b7055e75a59b7ecca
chanikya23 <chanikyasatish23@gmail.com>
2024-07-21 11:36:50 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,24 @@
+/* 
+<div id="parent">
+    <div id="child">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div>
+*/
+
+
+const heading1= React.createElement("h1",{},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"this h1 is child1"),
+        React.createElement("h2",{},"This is h2 child1")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"this is h1 from child2"),
+        React.createElement("h2",{},"this is h2 from child2")
+    ])
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading1);
