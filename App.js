b5a23c26fa63f0be43125ef007cb0a01a3b2612d
chanikya23 <chanikyasatish23@gmail.com>
2024-07-22 16:08:43 +0530

    episode3-JSX

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
-/* 
-<div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div>
-*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading1= React.createElement("h1",{},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"this h1 is child1 from the actual react"),
-        React.createElement("h2",{},"This is h2 child1")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"this is h1 from child2"),
-        React.createElement("h2",{},"this is h2 from child2")
-    ])
-])
+// const heading= React.createElement("h1", {}, "Namaste Chanikya!");
+// console.log(heading);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const jsxheading= <h1 id="heading">Namaste Chanikya from JSX!</h1>  
+// console.log(jsxheading);
+// const root = ReactDOM.createRoot(document.getElementById('root'))   ;
 
-root.render(heading1);
+
+// root.render(heading);
+
+//React Element
+const Title =()=>(
+    <h1 tableIndex='5'>
+        Namaste React using JSX, React Element.
+    </h1>
+)
+
+// const data=api.getData();
+    
+//React Functional Component & It is component composition.
+const HeadingComponent= () =>(
+    <div id='container'>
+    {Title()}
+    {/* <Title/> */}
+    <h1 className='heading'>React from the Functional component</h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
